500dddabc3d2f3a2d84b77521d51e05a6d7bdf76
Benny93 <vollmerbenjamin93@gmail.com>
2017-04-12 12:10:58 +0200

    Added install dependencys script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SDNInstallScripts
+
+curl -L -O
+raw.githubusercontent.com/Benny93/SDNInstallScripts/master/installOnos.sh
---
+++ installDependecies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+#java
+sudo apt-get install default-jre
+
+#python
+sudo apt-get install python3
+
+#pip
+sudo apt-get install python3-pip
