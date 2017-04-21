8e093d5aaf95190745e04e637a972d86891b0fb1
Benny93 <vollmerbenjamin93@gmail.com>
2017-04-21 10:07:57 +0200

    Added rc local for router

--- rclocal/rclocal_router.sh
+++ rclocal/rclocal_router.sh
@@ -4,3 +4,4 @@ sysctl -w net.ipv4.ip_forward=1
 ifconfig eth0 192.168.1.254 netmask 255.255.255.0 up
 ifconfig eth1 192.168.2.254 netmask 255.255.255.0 up
 ifconfig eth2 192.168.3.254 netmask 255.255.255.0 up
+ip r a 10.11.12.13 nexthop via 192.168.2.1 dev eth1 nexthop via 192.168.3.1 dev eth2
