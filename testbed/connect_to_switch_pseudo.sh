0e1e24717523da4cb752c84620247d5625e5e16f
Benny93 <vollmerbenjamin93@gmail.com>
2017-05-19 17:29:24 +0200

    Added connection scripts for second controller and switch

---
+++ testbed/connect_to_switch_pseudo.sh
@@ -0,0 +1,7 @@
+#greps ip from mac and connects
+MAC='00:19:d2:75:e0:20'
+lanscan='arp-scan --localnet --numeric --ignoredups | grep -E "([a-f0-9]{2}:){5}[a-f0-9]{2}"'
+echo $lanscan
+ip="$(eval $lanscan | grep  $MAC | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")"
+echo "connecting to "$ip
+ssh pi@${ip}
